d437dac56f2994c31880f6d97744e4fbbdf53c57
Akash-nath29 <anath5440@gmail.com>
2023-09-07 10:40:43 +0530

    Created RecipeFinder

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'MLXH243rjBDIBibiBIbibIUBImmfrdTWS7FDhdwYF56wPj8'
+
+db = SQLAlchemy(app)
+
+#TODO: make the database
+
+
+api_key = '21ce19a86415375dd63c0563138cd22a'
+
+# Base URL for Edamam Recipe Search API
+base_url = 'https://api.edamam.com/search'
+
+# Function to search for recipes
+def search_recipes(query):
+    params = {
+        'q': query,
+        'app_id': '0eae0989',
+        'app_key': api_key,
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    return data['hits']  # Return a list of recipe details
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        query = request.form.get('query')
+        recipes = search_recipes(query)
+        return render_template('index.html', recipes=recipes, query=query)
+    return render_template('index.html', recipes=None, query=None)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port='80')
